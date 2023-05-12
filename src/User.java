3ebf734b501b6b7367e7ffedd5e3da9013a6f84f
elsaD01 <cph-ed94@cphbusiness.dk>
2023-05-12 11:10:29 +0200

    fixed

--- src/User.java
+++ src/User.java
@@ -1,21 +1,28 @@
 public class User {
 
-    private String username;
+    private final String username;
 
-    private String password;
+    private final String password;
 
-    public User(String username, String password){
+    private String id;
+
+    public User(String username, String password, String id){
 
         this.username = username;
         this.password = password;
+        this.id = id;
     }
 
-    public String getUsername{
+    public String getUsername(){
         return username;
     }
 
     public String getPassword(){
         return password;
+    }
+
+    public String getId(){
+        return id;
     }
 
 
