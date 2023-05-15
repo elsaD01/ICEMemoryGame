ddcff86a13f96550ca0ec436e9d52191f233a267
SZA50 <125568354+SZA50@users.noreply.github.com>
2023-05-15 10:39:37 +0200

    undo

--- src/Cards.java
+++ src/Cards.java
@@ -1,16 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 
 public class Cards {
 
-   String [] cards1 = new String[10];
-   String [] cards2 = new String[20];
-   String [] cards3 = new String [30];
+   String [] card1 = new String[15];
+   String [] card2 = new String[20];
+   String [] card3 = new String [30];
 
-   public getMatch () {
-      return getMatch;
+   //public getMatch () {
+      //return getMatch;
+
+   public static void main (String[] args){
+     File file = new File("Card1.csv");
+     try {
+        Scanner inputStream = new Scanner(file);
+        while (inputStream.hasNext()) {
+            String data = inputStream.next();
+            System.out.println(data + "");
+                    }
+        inputStream.close();
+     } catch (FileNotFoundException e) {
+        e.printStackTrace();
+     }
+
 
    }
 
 
+
 }
+
+
+
